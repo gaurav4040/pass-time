37b69d00dd1045033824c5b36f99e41097695ac8
gaurav4040 <121619791+gaurav4040@users.noreply.github.com>
2024-12-20 12:29:48 +0000

    nothing

--- pass-comp/src/Random.jsx
+++ pass-comp/src/Random.jsx
@@ -1,6 +1,6 @@
 function Random(){
     let r_number = Math.random()*10;
-    return <h1 style={{'background-color':'#677002'}}>
+    return <h1 style={{'background-color':'#677102'}}>
         Random number is : {Math.round(r_number)} 
     </h1>
 }
