61c9b458895b315b824f94fa7b395db6765a9c96
gaurav4040 <121619791+gaurav4040@users.noreply.github.com>
2024-12-20 13:14:01 +0000

    new

---
+++ bootstrappass/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <div>
+    <button type="button" class="btn btn-primary">primary</button>
+    </div>
+}
+
+export default App;
